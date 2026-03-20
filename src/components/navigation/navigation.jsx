c58044e096fd4b4d00e883c322adeb3f2104b9f5
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-03-20 21:35:49 +0600

    An amazing pop up menu is builded

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,33 +1,45 @@
-import { TextAlignCenter, X } from "lucide-react";
+import { Link, TextAlignCenter, X } from "lucide-react";
 import React, { use, useState } from "react";
 
 const Navbar = ({ navigationData }) => {
-  const [open, setOpen] = useState(false)
-  function handleOpen(){
-    setOpen(!open)
+  const [open, setOpen] = useState(false);
+  function handleOpen() {
+    setOpen(!open);
   }
 
   const navData = use(navigationData);
+
   const navigation = navData.navigation;
 
-  return (
-    <nav className="flex justify-between m-3">
+  const Links = navigation.map((route, index) => (
+    <li key={index} className=" hover:bg-amber-500">
+      <a href={route.url}>{route.title}</a>
+    </li>
+  ));
 
-      <div onClick={handleOpen} className="flex gap-2">
-        {open ? <X className="md:hidden"></X> : <TextAlignCenter className="md:hidden"></TextAlignCenter> }
-        
+  return (
+    <nav className="flex justify-between m-3 text-lg">
+      <div onClick={handleOpen} className="flex gap-2 relative">
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <TextAlignCenter className="md:hidden"></TextAlignCenter>
+        )}
+
+        <ul
+          className={`md:hidden absolute 
+            ${open ? "top-9" : "-top-50"} duration-200
+            bg-amber-400 p-2 space-y-1.5 rounded-sm`}
+        >
+          {Links}
+        </ul>
+
         <h3>My Navbar</h3>
       </div>
 
-      <ul className="flex ">
-        {navigation.map((route, index) => (
-          <li key={index} className="mr-3">
-            <a href={route.url}>{route.title}</a>
-          </li>
-        ))}
-      </ul>
+      <ul className="hidden md:flex gap-5">{Links}</ul>
 
-      <button className="btn btn-primary">Log in</button>
+      <button className="btn btn-warning">Log in</button>
     </nav>
   );
 };
