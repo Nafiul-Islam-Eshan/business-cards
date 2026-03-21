3bc6b2c85fad2a03e34dbdda2edc5fdeef53e241
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-03-21 07:09:41 +0600

    Refurnished

--- src/PricingOptions/PricingOptions.jsx
+++ src/PricingOptions/PricingOptions.jsx
@@ -7,7 +7,7 @@ const PricingOptions = ({ pricingOptionsData }) => {
   console.log(memberPlanPriceOptions);
 
   return (
-    <div className="space-y-11">
+    <div className="space-y-11 p-8">
       <h2 className="text-4xl font-bold">All Membership Plans</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9">
         {memberPlanPriceOptions.map((memberPlanPriceOption, index) => (
--- src/components/PricingCards/PricingCards.jsx
+++ src/components/PricingCards/PricingCards.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import { CircleCheckBig } from "lucide-react";
+import React from "react";
 
-const PricingCards = ({memberPlanPriceOption}) => {
-    console.log(memberPlanPriceOption);
-    const {title, price, currency, duration, features} = memberPlanPriceOption
+const PricingCards = ({ memberPlanPriceOption }) => {
+  console.log(memberPlanPriceOption);
+  const { title, price, currency, duration, features } = memberPlanPriceOption;
 
-    return (
-        <div className='border-2 bg-amber-400 shadow-2xl rounded-lg p-4 space-y-7'>
-            {/* Header */}
-            <header>
-                <h1 className="text-4xl font-bold">{title} <span className="font-semibold">({duration})</span> </h1>
-                <h3 className="text-3xl">{price} {currency}</h3>
-            </header>
+  return (
+    <div className="border-2 bg-amber-400 shadow-2xl rounded-lg p-4 space-y-7">
+      {/* Header */}
+      <header>
+        <h1 className="text-4xl font-bold">
+          {title} <span className="font-semibold">({duration})</span>{" "}
+        </h1>
+        <h3 className="text-3xl">
+          {price} {currency}/Month
+        </h3>
+      </header>
 
-            {/* Featueres */}
-            <main className="space-y-2">
-                <h2 className="text-3xl font-bold underline decoration-wavy">Features : </h2>
-                <ol className='list-decimal text-xl ml-11'>
-                    {
-                        features.map((feature, index) => <li key={index} >{feature}</li> )
-                    }
-                </ol>
-            </main>
-        </div>
-    );
+      {/* Featueres */}
+      <main className="space-y-2">
+        <h2 className="text-3xl font-bold underline decoration-wavy">
+          Features :{" "}
+        </h2>
+        <ol className="text-xl ml-11 space-y-2 text-blue-600">
+          {features.map((feature, index) => (
+            <li key={index} className="flex items-center gap-1"> <CircleCheckBig className="text-blue-600"></CircleCheckBig> {feature}</li>
+          ))}
+        </ol>
+      </main>
+    </div>
+  );
 };
 
 export default PricingCards;
